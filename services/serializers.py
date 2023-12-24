a9fa6d03075f98ae0b0e429402aae52db4b54d76
DESKTOP-VGH69S8\AlexeyShakov <alexei_96@inbox.ru>
2023-12-24 10:57:13 +0300

    added field counted in real time

--- services/serializers.py
+++ services/serializers.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers
 
-from .models import Subscription
+from .models import Subscription, Plan, Service
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ("id", "discount_percent")
+
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ("id", "full_price")
 
 
 class SubscriptionSerializers(serializers.ModelSerializer):
+    plan = PlanSerializer()
+    service = ServiceSerializer()
     client_name = serializers.CharField(source="client.company_name")
     email = serializers.CharField(source="client.user.email")
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance: Subscription):
+        return instance.price
 
     class Meta:
         model = Subscription
-        fields = ("id", "plan_id", "client_name", "email")
+        fields = ("id", "plan_id", "client_name", "email", "price", "plan", "service")
--- services/views.py
+++ services/views.py
@@ -1,3 +1,4 @@
+from django.db.models import F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from services.models import Subscription
@@ -5,5 +6,10 @@ from services.serializers import SubscriptionSerializers
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.select_related("client", "client__user").only("id", "plan_id", "client__company_name", "client__user__email")
+    queryset = Subscription.objects.select_related("client", "client__user", "service", "plan"). \
+        only("id", "plan_id", "client__company_name", "client__user__email", "service__full_price",
+             "plan__discount_percent").\
+        annotate(
+        price=F("service__full_price") - F("service__full_price") * F("plan__discount_percent") / 100
+    )
     serializer_class = SubscriptionSerializers
